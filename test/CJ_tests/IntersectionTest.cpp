e4a255fd50bf342e69638cd7d1e7ddf913bd7e04
ashybabashyba <cramoss@unal.edu.co>
2024-08-14 23:28:19 -0500

    Added computation of intersection types using triangles and segments

--- test/CJ_tests/IntersectionTest.cpp
+++ test/CJ_tests/IntersectionTest.cpp
@@ -22,7 +22,8 @@ typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_3 Point_3;
 typedef Kernel::Plane_3 Plane_3;
 typedef Kernel::Segment_3 Segment_3;
-typedef boost::variant<Segment_3, Point_3, std::vector<Point_3>> IntersectResult;
+typedef Kernel::Triangle_3 Triangle_3;
+typedef boost::variant<Segment_3, Point_3, std::vector<Point_3>, std::vector<Segment_3>> IntersectResult;
 
 
 class IntersectionTest : public ::testing::Test {
@@ -70,44 +71,83 @@ public:
         return Mesh{ grid, coords, groups };
     }
 
-    static std::vector<Plane_3> defineCubePlanes() {
+    static std::vector<Triangle_3> defineCubeTriangles() {
         Mesh cube = buildCubeMesh();
         return {
-            Plane_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]), 
-                    Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
-                    Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
+            Triangle_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]),
+                       Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
+                       Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
 
-            Plane_3(Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2]),
-                    Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2]),
-                    Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2])),
+            Triangle_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]),
+                       Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2]),
+                       Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
 
-            Plane_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]),
-                    Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2]),
-                    Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
+            Triangle_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]),
+                       Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
+                       Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2])),
 
-            Plane_3(Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
-                    Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2]),
-                    Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2])),
+            Triangle_3(Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2]),
+                       Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
+                       Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2])),
 
-            Plane_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]),
-                    Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
-                    Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2])),
+            Triangle_3(Point_3(cube.coordinates[0][0], cube.coordinates[0][1], cube.coordinates[0][2]),
+                       Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2]),
+                       Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2])),
 
-            Plane_3(Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2]),
-                    Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2]),
-                    Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2]))
-        };
+            Triangle_3(Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2]),
+                       Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2]),
+                       Point_3(cube.coordinates[7][0], cube.coordinates[7][1], cube.coordinates[7][2])),
+
+            Triangle_3(Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2]),
+                       Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2]),
+                       Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
+
+            Triangle_3(Point_3(cube.coordinates[7][0], cube.coordinates[7][1], cube.coordinates[7][2]),
+                       Point_3(cube.coordinates[3][0], cube.coordinates[3][1], cube.coordinates[3][2]),
+                       Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2])),
+
+            Triangle_3(Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2]),
+                       Point_3(cube.coordinates[1][0], cube.coordinates[1][1], cube.coordinates[1][2]),
+                       Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
+
+            Triangle_3(Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2]),
+                       Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2]),
+                       Point_3(cube.coordinates[2][0], cube.coordinates[2][1], cube.coordinates[2][2])),
+
+            Triangle_3(Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2]),
+                       Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2]),
+                       Point_3(cube.coordinates[4][0], cube.coordinates[4][1], cube.coordinates[4][2])),
+
+            Triangle_3(Point_3(cube.coordinates[5][0], cube.coordinates[5][1], cube.coordinates[5][2]),
+                       Point_3(cube.coordinates[6][0], cube.coordinates[6][1], cube.coordinates[6][2]),
+                       Point_3(cube.coordinates[7][0], cube.coordinates[7][1], cube.coordinates[7][2])),
+
+                    };
     }
 
-    static boost::optional<IntersectResult> findMaxIntersection(const Segment_3& segment, const std::vector<Plane_3>& planes) {
+    static Segment_3 unite_collinear_segments(const Segment_3& s1, const Segment_3& s2) {
+        if (!CGAL::collinear(s1.source(), s1.target(), s2.source()) ||
+            !CGAL::collinear(s1.source(), s1.target(), s2.target())) {
+            throw std::invalid_argument("The segments are not collinear.");
+        }
+
+        Point_3 min_point = std::min({ s1.source(), s1.target(), s2.source(), s2.target() });
+        Point_3 max_point = std::max({ s1.source(), s1.target(), s2.source(), s2.target() });
+
+        return Segment_3(min_point, max_point);
+    }
+
+    static boost::optional<IntersectResult> findMaxIntersection(const Segment_3& segment, const std::vector<Triangle_3>& triangles) {
         std::set<Point_3> intersection_points;
+        std::vector<Segment_3> intersection_segments; 
         boost::optional<IntersectResult> max_intersection;
 
-        for (const auto& plane : planes) {
-            auto result = CGAL::intersection(segment, plane);
+        for (const auto& triangle : triangles) {
+            auto result = CGAL::intersection(segment, triangle);
             if (result) {
                 if (const Segment_3* l = boost::get<Segment_3>(&*result)) {
-                    return *l;
+                    Segment_3 segment_to_store = *l;
+                    intersection_segments.push_back(segment_to_store); 
                 }
                 else if (const Point_3* p = boost::get<Point_3>(&*result)) {
                     intersection_points.insert(*p);
@@ -117,9 +157,19 @@ public:
                 }
             }
         }
-        if (!intersection_points.empty()) {
+        if (!intersection_points.empty() && intersection_segments.empty()) {
             std::vector<Point_3> unique_points(intersection_points.begin(), intersection_points.end());
             return unique_points; 
+        }
+        if (!intersection_segments.empty()) {
+            auto it = intersection_segments.begin(); 
+            Segment_3 s1 = *it; 
+
+            ++it; 
+            Segment_3 s2 = *it; 
+
+            Segment_3 united_segment = unite_collinear_segments(s1, s2); 
+            return united_segment; 
         }
         return max_intersection;
     }
@@ -127,11 +177,11 @@ public:
 
 TEST_F(IntersectionTest, CubeFaceIntersections) {
     Mesh cube = buildCubeMesh();
-    Segment_3 segment(Point_3(0.5, 0.5, -1), Point_3(0.5, 0.5, 0.5));
+    Segment_3 segment(Point_3(0.5, 0, 0), Point_3(0.5, 0, 4));
 
-    std::vector<Plane_3> planes = defineCubePlanes();
+    std::vector<Triangle_3> triangles = defineCubeTriangles();
 
-    auto max_intersection = findMaxIntersection(segment, planes);
+    auto max_intersection = findMaxIntersection(segment, triangles);
 
     if (max_intersection) {
         if (const Segment_3* l = boost::get<Segment_3>(&*max_intersection)) {
